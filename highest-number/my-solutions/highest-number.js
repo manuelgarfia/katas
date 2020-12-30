8dfeeed0498835d8a817ff164c7f5b6132091222
Manolo Garfia <manuelgarfiaguerrero@hotmail.com>
2020-12-30 12:53:34 +0100

    Kata highest-number has been completed modified to for of

--- highest-number/my-solutions/highest-number.js
+++ highest-number/my-solutions/highest-number.js
@@ -2,12 +2,11 @@ export function getHighestNumber(array)
 {
   let highestnumber=0
 
-  for (let i = 0; i < array.length; i++) {
-    if (highestnumber <= array[i]) {
-      highestnumber= array[i]
+
+    for (let arrayElement of array) {
+      if (highestnumber <= arrayElement) highestnumber= arrayElement
     }
-
-  }
 return  highestnumber
+  }
 
-}
+
